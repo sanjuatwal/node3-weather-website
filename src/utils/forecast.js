a2c73c0ecad2aaaa7223bc8b6c9e08d114662081
unknown <sanjuatwal007@gmail.com>
2021-02-04 04:07:49 +0530

    changing the forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find Location!', undefined)
         } else{
-            callback(undefined, body.current.weather_descriptions + ". It is currenty " + body.current.temperature + " degrees out. There is a " + body.current.precip + "% chance of rain.")
+            callback(undefined, body.current.weather_descriptions + ". It is currenty " + body.current.temperature + " degrees out and it feels like " + body.current.feelslike + " degrees." +  "There is a " + body.current.precip + "% chance of rain.")
         }
     })
 }
